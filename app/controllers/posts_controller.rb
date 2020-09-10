9d8972db570e4f58c2d57fc5c72cc4d483eeedc9
kero-arboreus <0yuka.odachi0@gmail.com>
2020-09-10 15:39:22 +0900

    メモ投稿機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:content], checked: false)
+    post = Post.create(content: params[:content], checked: false)
     render json:{ post: post }
   end
 
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
       post.update(checked: true)
     end
     item = Post.find(params[:id])
-    render json: { post: item }
+    render json:{ post: item }
   end
   
 end
